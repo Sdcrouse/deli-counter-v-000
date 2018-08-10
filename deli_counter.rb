521ed0b52a94dcdd0ec13b1df2d3709a98250a08
Steven Crouse <sdcrouse91@gmail.com>
2018-08-10 21:59:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,7 @@ def line(customers)
   if customers.size == 0 
     puts "The line is currently empty."
   else
-    line_message = 
+    line_order = 0 
+    puts "The line is currently: " + line_order
   end
 end
