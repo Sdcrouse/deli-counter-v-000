f2d3fc92637fb6bf4f81abb0c9f3da3c6fd1d60d
Steven Crouse <sdcrouse91@gmail.com>
2018-08-10 22:40:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ end
 
 def take_a_number(katz_deli, customer)
   katz_deli.push(customer)
-  #puts "Welcome, #{customer}. You are number #{katz_deli.size} in line."
+  puts "Welcome, #{customer}. You are number #{katz_deli.size} in line."
   
   #message = "Welcome, #{customer}. You are number #{katz_deli.size} in line."
   #binding.pry
